9e9f7526ccb2aeb22d9bfd70a7178c560acca6a8
AlessP93 <alespiscino@libero.it>
2022-05-13 16:11:24 +0200

    aggiunta struttura

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const post = [
+    {
+        nome: "Wayne Barnett",
+        img: "img/wayne-barnett-founder-ceo.jpg",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Angela Caroll",
+        img: "img/angela-caroll-chief-editor.jpg",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Walter Gordon",
+        img: "img/walter-gordon-office-manager.jpg",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Wayne Barnett",
+        img: "img/angela-lopez-social-media-manager.jpg",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Wayne Barnett",
+        img: "img/scott-estrada-developer.jpg",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Wayne Barnett",
+        img: "img/barbara-ramos-graphic-designer.jpg",
+        ruolo: "Founder & CEO",
+    }
+
+];
+
